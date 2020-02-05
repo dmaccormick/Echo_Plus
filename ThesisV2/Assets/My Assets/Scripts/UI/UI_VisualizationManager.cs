0bb1074a9e45985165e38570b75c5b3fd98840b1
Dan <dmaccormick@hotmail.com>
2020-02-05 16:51:40 -0500

    Started to create the visualization manager UI
    -  Similar to the recording manager UI layout but different controls
    - Removed the UI prefab for the recording manager since the changes were not getting set as it was nested

---
+++ ThesisV2/Assets/My Assets/Scripts/UI/UI_VisualizationManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Thesis.Visualization;
+
+namespace Thesis.UI
+{
+    public class UI_VisualizationManager : MonoBehaviour
+    {
+        //--- Public Variables ---//
+        [Header("Controls")]
+        public Visualization_Manager m_visManager;
+
+        [Header("Toolbar UI Elements")]
+        public Button m_btnOpenSettings;
+
+        [Header("Settings UI Elements")]
+        public GameObject m_pnlSettings;
+
+
+
+        //--- Toolbar Callbacks ---//
+        public void OnToggleSettings()
+        {
+            // Toggle the settings UI panel
+            bool isPanelActive = m_pnlSettings.gameObject.activeSelf;
+            m_pnlSettings.gameObject.SetActive(!isPanelActive);
+        }
+    }
+}
