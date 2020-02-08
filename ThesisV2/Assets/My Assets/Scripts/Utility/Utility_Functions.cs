35729300182ef63b96167c882f8cacb0e548e66c
Dan <dmaccormick@hotmail.com>
2020-02-08 13:59:30 -0500

    Implemented parsing for lifetime and renderables tracks
    - Added a parsing function for Colour's
    -  Lifetime and renderable tracks now parse data points without error

--- ThesisV2/Assets/My Assets/Scripts/Utility/Utility_Functions.cs
+++ ThesisV2/Assets/My Assets/Scripts/Utility/Utility_Functions.cs
@@ -53,6 +53,28 @@ namespace Thesis.Utility
             return newVec;
         }
 
+        public static Color ParseColor(string _str)
+        {
+            // Start by removing the parentheses
+            int openBracketIndex = _str.IndexOf('(') + 1;
+            int closeBracketIndex = _str.IndexOf(')');
+            int substrLength = closeBracketIndex - openBracketIndex;
+            _str = _str.Substring(openBracketIndex, substrLength);
+
+            // Split the rest of the string on the commas to get the individual floats
+            string[] tokens = _str.Split(',');
+
+            // Create a new colour and parse the individual floats into it
+            Color newColor = new Color();
+            newColor.r = float.Parse(tokens[0]);
+            newColor.g = float.Parse(tokens[1]);
+            newColor.b = float.Parse(tokens[2]);
+            newColor.a = float.Parse(tokens[3]);
+
+            // Return the created colour
+            return newColor;
+        }
+
         public static Quaternion ParseQuaternion(string _str)
         {
             // Start by removing the parentheses
@@ -64,14 +86,14 @@ namespace Thesis.Utility
             // Split the rest of the string on the commas to get the individual floats
             string[] tokens = _str.Split(',');
 
-            // Create a new vector3 and parse the individual floats into it
+            // Create a new quaternion and parse the individual floats into it
             Quaternion newQuat = new Quaternion();
             newQuat.x = float.Parse(tokens[0]);
             newQuat.y = float.Parse(tokens[1]);
             newQuat.z = float.Parse(tokens[2]);
             newQuat.w = float.Parse(tokens[3]);
 
-            // Return the created vector
+            // Return the created quaternion
             return newQuat;
         }
     }
--- ThesisV2/Assets/My Assets/Scripts/VisTrack/VisTrack_Renderables.cs
+++ ThesisV2/Assets/My Assets/Scripts/VisTrack/VisTrack_Renderables.cs
@@ -1,15 +1,88 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
 using Thesis.Interface;
+using Thesis.Utility;
 
 namespace Thesis.VisTrack
 {
     public class VisTrack_Renderables : MonoBehaviour, IVisualizable
     {
+        //--- Data Struct ---//
+        public struct Data_Renderables
+        {
+            public Data_Renderables(string _dataStr)
+            {
+                // Split the data string
+                string[] tokens = _dataStr.Split('~');
+
+                // The first token is the timestamp so just parse the float
+                m_timestamp = float.Parse(tokens[0]);
+
+                // The second token is the path to the mesh so we need to get the mesh itself from the asset database
+                m_mesh = AssetDatabase.LoadAssetAtPath(tokens[1], typeof(Mesh)) as Mesh;
+
+                // The third token is the path to the material so load that too
+                m_material = AssetDatabase.LoadAssetAtPath(tokens[2], typeof(Material)) as Material;
+
+                // The fourth token is the colour which is a vector3 so parse that
+                m_color = Utility_Functions.ParseColor(tokens[3]);
+            }
+
+            public static List<Data_Renderables> ParseDataList(string _data)
+            {
+                // Create a list to hold the parsed data
+                List<Data_Renderables> dataPoints = new List<Data_Renderables>();
+
+                // Split the string into individual lines which each are one data point
+                string[] lines = _data.Split('\n');
+
+                // Create new data points from each of the lines
+                foreach (string line in lines)
+                {
+                    // If the line is empty, do nothing
+                    if (line == null || line == "")
+                        continue;
+
+                    // Otherwise, create a new data point
+                    dataPoints.Add(new Data_Renderables(line));
+                }
+
+                // Return the list of data points
+                return dataPoints;
+            }
+
+            public float m_timestamp;
+            public Mesh m_mesh;
+            public Material m_material;
+            public Color m_color;
+        }
+
+
+
+        //--- Private Variables ---//
+        private List<Data_Renderables> m_dataPoints;
+
+
+
         //--- IVisualizable Interface ---// 
         public bool InitWithString(string _data)
         {
-            //throw new System.NotImplementedException();
-            return true;
+            try
+            {
+                // Create a list of data points by parsing the string
+                m_dataPoints = Data_Renderables.ParseDataList(_data);
+
+                // If everything worked correctly, return true
+                return true;
+            }
+            catch (Exception _e)
+            {
+                // If something went wrong, output an error and return false
+                Debug.LogError("Error in InitWithString(): " + _e.Message);
+                return false;
+            }
         }
 
         public void StartVisualization()
