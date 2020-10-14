79238ae5af116ebc02f781e530f9cdfec6d096b5
Dan <dmaccormick@hotmail.com>
2020-10-14 10:09:14 -0400

    Added a light track (both rec and vis)
    - Records the light's type, its intensity, and its colour
    > NOTE: this means that it currently only fully supports directional lights 
    > Need to add spot angle and range for the other types to be supported

--- ThesisV2/Assets/Thesis/My Assets/Scripts/RecTrack/RecTrack_Light.cs
+++ ThesisV2/Assets/Thesis/My Assets/Scripts/RecTrack/RecTrack_Light.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEditor;
 using Thesis.Recording;
 using Thesis.Interface;
 using System.Collections.Generic;
@@ -8,47 +7,50 @@ using System.Text;
 
 namespace Thesis.RecTrack
 {
-    // Currently only fully works with perspective cameras
-    // TODO: Add full support for ortho cameras!
+    // Currently only fully works with directional lights
+    // TODO: Add full support for point and spot lights!
     [RequireComponent(typeof(Recording_Object))]
-    public class RecTrack_Camera : MonoBehaviour, IRecordable
+    public class RecTrack_Light : MonoBehaviour, IRecordable
     {
         //--- Data Struct ---//
         [System.Serializable]
-        public struct Data_Camera
+        public struct Data_Light
         {
-            public Data_Camera(float _timestamp, float _fov, float _clipClose, float _clipFar)
+            public Data_Light(float _timestamp, LightType _type, Color _colour, float _intensity)
             {
                 this.m_timestamp = _timestamp;
-                this.m_fov = _fov;
-                this.m_clipClose = _clipClose;
-                this.m_clipFar = _clipFar;
+                this.m_type = _type;
+                this.m_colour = _colour;
+                this.m_intensity = _intensity;
             }
 
             public string GetString(string _format)
             {
-                return this.m_timestamp.ToString(_format) + "~" + m_fov.ToString() + "~" + m_clipClose.ToString() + "~" + m_clipFar.ToString();
+                return this.m_timestamp.ToString(_format) + "~" 
+                    + ((int)this.m_type).ToString() + "~"
+                    + this.m_colour.ToString(_format) + "~"
+                    + this.m_intensity.ToString();
             }
 
             public float m_timestamp;
-            public float m_fov;
-            public float m_clipClose;
-            public float m_clipFar;
+            public LightType m_type;
+            public Color m_colour;
+            public float m_intensity;
         }
 
 
 
         //--- Public Variables ---//
-        public Camera m_targetCam;
+        public Light m_targetLight;
         public string m_dataFormat = "F3";
 
 
 
         //--- Private Variables ---//
-        private List<Data_Camera> m_dataPoints;
-        private float m_currentFov;
-        private float m_currentClipClose;
-        private float m_currentClipFar;
+        private List<Data_Light> m_dataPoints;
+        private LightType m_currentType;
+        private Color m_currentColour;
+        private float m_currentIntensity;
 
 
 
@@ -56,13 +58,13 @@ namespace Thesis.RecTrack
         public void StartRecording(float _startTime)
         {
             // Ensure the targets are not null
-            Assert.IsNotNull(m_targetCam, "m_targetCam needs to be set for the track on object [" + this.gameObject.name + "]");
+            Assert.IsNotNull(m_targetLight, "m_targetLight needs to be set for the track on object [" + this.gameObject.name + "]");
 
             // Init the private variables 
-            m_dataPoints = new List<Data_Camera>();
-            m_currentFov = m_targetCam.fieldOfView;
-            m_currentClipClose = m_targetCam.nearClipPlane;
-            m_currentClipFar = m_targetCam.farClipPlane;
+            m_dataPoints = new List<Data_Light>();
+            m_currentType = m_targetLight.type;
+            m_currentColour = m_targetLight.color;
+            m_currentIntensity = m_targetLight.intensity;
 
             // Record the first data point
             RecordData(_startTime);
@@ -76,15 +78,15 @@ namespace Thesis.RecTrack
 
         public void UpdateRecording(float _currentTime)
         {
-            // If any of the camera settings have changed, update the values and record the change
-            if (m_currentFov != m_targetCam.fieldOfView ||
-                m_currentClipClose != m_targetCam.nearClipPlane ||
-                m_currentClipFar != m_targetCam.farClipPlane)
+            // If any of the light settings have changed, update the values and record the change
+            if (m_currentType != m_targetLight.type ||
+                m_currentColour != m_targetLight.color ||
+                m_currentIntensity != m_targetLight.intensity)
             {
                 // Update the values
-                m_currentFov = m_targetCam.fieldOfView;
-                m_currentClipClose = m_targetCam.nearClipPlane;
-                m_currentClipFar = m_targetCam.farClipPlane;
+                m_currentType = m_targetLight.type;
+                m_currentColour = m_targetLight.color;
+                m_currentIntensity = m_targetLight.intensity;
 
                 // Record the changes to the values
                 RecordData(_currentTime);
@@ -97,7 +99,7 @@ namespace Thesis.RecTrack
             Assert.IsNotNull(m_dataPoints, "m_dataPoints must be init before calling RecordData() on object [" + this.gameObject.name + "]");
 
             // Add a new data point to the list
-            m_dataPoints.Add(new Data_Camera(_currentTime, m_currentFov, m_currentClipClose, m_currentClipFar));
+            m_dataPoints.Add(new Data_Light(_currentTime, m_currentType, m_currentColour, m_currentIntensity));
         }
 
         public string GetData()
@@ -109,7 +111,7 @@ namespace Thesis.RecTrack
             StringBuilder stringBuilder = new StringBuilder();
 
             // Add all of the datapoints to the string
-            foreach (Data_Camera data in m_dataPoints)
+            foreach (Data_Light data in m_dataPoints)
                 stringBuilder.AppendLine("\t\t" + data.GetString(m_dataFormat));
 
             // Return the full set of data grouped together
@@ -118,17 +120,17 @@ namespace Thesis.RecTrack
 
         public string GetTrackName()
         {
-            return "Camera";
+            return "Light";
         }
 
         public void SetupDefault()
         {
             // Setup this recording track by grabbing default values from this object
-            m_targetCam = GetComponent<Camera>();
+            m_targetLight = GetComponent<Light>();
 
             // If either one failed, try to grab from the children or the parent instead
-            m_targetCam = (m_targetCam == null) ? GetComponentInChildren<Camera>() : m_targetCam;
-            m_targetCam = (m_targetCam == null) ? GetComponentInParent<Camera>() : m_targetCam;
+            m_targetLight = (m_targetLight == null) ? GetComponentInChildren<Light>() : m_targetLight;
+            m_targetLight = (m_targetLight == null) ? GetComponentInParent<Light>() : m_targetLight;
         }
     }
 }
---
+++ ThesisV2/Assets/Thesis/My Assets/Scripts/VisTrack/VisTrack_Light.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Thesis.Interface;
+using Thesis.Utility;
+
+namespace Thesis.VisTrack
+{
+    [RequireComponent(typeof(Light))]
+    public class VisTrack_Light : MonoBehaviour, IVisualizable
+    {
+        //--- Data Struct ---//
+        public struct Data_Light
+        {
+            public Data_Light(string _dataStr)
+            {
+                // Split the data string
+                string[] tokens = _dataStr.Split('~');
+
+                // The first token is the timestamp so just parse the float
+                m_timestamp = float.Parse(tokens[0]);
+
+                // The second token is the type of light
+                m_type = (LightType)int.Parse(tokens[1]);
+
+                // The third token is the colour which we need to parse using the utility function
+                m_colour = Utility_Functions.ParseColor(tokens[2]);
+
+                // The fourth token is the light intensity
+                m_intensity = float.Parse(tokens[3]);
+            }
+
+            public static List<Data_Light> ParseDataList(string _data)
+            {
+                // Create a list to hold the parsed data
+                List<Data_Light> dataPoints = new List<Data_Light>();
+
+                // Split the string into individual lines which each are one data point
+                string[] lines = _data.Split('\n');
+
+                // Create new data points from each of the lines
+                foreach (string line in lines)
+                {
+                    // If the line is empty, do nothing
+                    if (line == null || line == "")
+                        continue;
+
+                    // Otherwise, create a new data point
+                    dataPoints.Add(new Data_Light(line));
+                }
+
+                // Return the list of data points
+                return dataPoints;
+            }
+
+            public float m_timestamp;
+            public LightType m_type;
+            public Color m_colour;
+            public float m_intensity;
+        }
+
+
+
+        //--- Private Variables ---//
+        private Light m_targetLight;
+        private List<Data_Light> m_dataPoints;
+
+
+
+        //--- IVisualizable Interface ---// 
+        public bool InitWithString(string _data)
+        {
+            try
+            {
+                // Create a list of data points by parsing the string
+                m_dataPoints = Data_Light.ParseDataList(_data);
+
+                // If everything worked correctly, return true
+                return true;
+            }
+            catch (Exception _e)
+            {
+                // If something went wrong, output an error and return false
+                Debug.LogError("Error in InitWithString(): " + _e.Message);
+                return false;
+            }
+        }
+
+        public void StartVisualization(float _startTime)
+        {
+            // Init the targets
+            m_targetLight = GetComponent<Light>();
+
+            // Apply the initial visualization
+            UpdateVisualization(_startTime);
+        }
+
+        public void UpdateVisualization(float _time)
+        {
+            // Get the relevant data point for the given time
+            int dataIdx = FindDataPointForTime(_time);
+            Data_Light dataPoint = m_dataPoints[dataIdx];
+
+            // Apply the data point to the visualization
+            m_targetLight.type = dataPoint.m_type;
+            m_targetLight.color = dataPoint.m_colour;
+            m_targetLight.intensity = dataPoint.m_intensity;
+        }
+
+        public int FindDataPointForTime(float _time)
+        {
+            // Ensure the datapoints are actually setup
+            Assert.IsNotNull(m_dataPoints, "m_dataPoints has to be setup for before looking for a data point on object [" + this.gameObject.name + "]");
+            Assert.IsTrue(m_dataPoints.Count >= 1, "m_dataPoints cannot be empty on object [" + this.gameObject.name + "]");
+
+            // Start by setting the selected index to 0 in case there is only one point
+            int selectedIndex = 0;
+
+            // Loop through all of the data and find the nearest point BEFORE the given time
+            for (selectedIndex = 0; selectedIndex < m_dataPoints.Count - 1; selectedIndex++)
+            {
+                // Get the datapoint at the current index and next index
+                var thisDataPoint = m_dataPoints[selectedIndex];
+                var nextDataPoint = m_dataPoints[selectedIndex + 1];
+
+                // If this datapoint is BEFORE OR AT the time and the next one is AFTER the time, then we are at the right data point
+                if (thisDataPoint.m_timestamp <= _time && nextDataPoint.m_timestamp > _time)
+                    break;
+            }
+
+            // Return the selected index
+            return selectedIndex;
+        }
+
+        public string GetTrackName()
+        {
+            return "Light";
+        }
+
+        public float GetFirstTimestamp()
+        {
+            // Ensure the datapoints are actually setup
+            Assert.IsNotNull(m_dataPoints, "m_dataPoints has to be setup for before looking for a data point on object [" + this.gameObject.name + "]");
+            Assert.IsTrue(m_dataPoints.Count >= 1, "m_dataPoints cannot be empty on object [" + this.gameObject.name + "]");
+
+            // Return the timestamp for the first data point
+            return m_dataPoints[0].m_timestamp;
+        }
+
+        public float GetLastTimestamp()
+        {
+            // Ensure the datapoints are actually setup
+            Assert.IsNotNull(m_dataPoints, "m_dataPoints has to be setup for before looking for a data point on object [" + this.gameObject.name + "]");
+            Assert.IsTrue(m_dataPoints.Count >= 1, "m_dataPoints cannot be empty on object [" + this.gameObject.name + "]");
+
+            // Return the timestamp for the last data point
+            return m_dataPoints[m_dataPoints.Count - 1].m_timestamp;
+        }
+    }
+}
