2483526c2a3b0ba8569cf2d04c1c4fc196a30fb5
Dan <dmaccormick@hotmail.com>
2020-12-15 21:39:17 -0500

    Updated all vis tracks to use a more efficient datapoint finding algo
    - The previous one was always a linear search from the first datapoint...meaning it took longer as the vis moved on since it had to search further
    - Now algo starts at the last datapoint and will search either forward or backward depending on the difference in time
    - Also now has a better cap if the end of the timeline is reached

--- ThesisV2/Assets/Thesis/My Assets/Scripts/VisTrack/VisTrack_Position.cs
+++ ThesisV2/Assets/Thesis/My Assets/Scripts/VisTrack/VisTrack_Position.cs
@@ -58,6 +58,8 @@ namespace Thesis.VisTrack
         //--- Private Variables ---//
         private Transform m_targetTransform;
         private List<Data_Position> m_dataPoints;
+        private int m_lastDataIndex = 0;
+        private float m_lastTime = 0.0f;
 
 
 
@@ -125,23 +127,113 @@ namespace Thesis.VisTrack
             Assert.IsNotNull(m_dataPoints, "Track Assert Failed [" + GetTrackName() + "] - " + "m_dataPoints has to be setup for before looking for a data point on object [" + this.gameObject.name + "]");
             Assert.IsTrue(m_dataPoints.Count >= 1, "Track Assert Failed [" + GetTrackName() + "] - " + "m_dataPoints cannot be empty on object [" + this.gameObject.name + "]");
 
-            // Start by setting the selected index to 0 in case there is only one point
-            int selectedIndex = 0;
-
-            // Loop through all of the data and find the nearest point BEFORE the given time
-            for (selectedIndex = 0; selectedIndex < m_dataPoints.Count - 1; selectedIndex++)
+            if (m_dataPoints.Count > 1)
             {
-                // Get the datapoint at the current index and next index
-                var thisDataPoint = m_dataPoints[selectedIndex];
-                var nextDataPoint = m_dataPoints[selectedIndex + 1];
+                // If we are PAST the last data point, return the final datapoint again
+                if (_time >= m_dataPoints[m_dataPoints.Count - 1].m_timestamp)
+                    return m_dataPoints.Count - 1;
 
-                // If this datapoint is BEFORE OR AT the time and the next one is AFTER the time, then we are at the right data point
-                if (thisDataPoint.m_timestamp <= _time && nextDataPoint.m_timestamp > _time)
-                    break;
+                // Determine if we are going forward or backward in time
+                float timeDiff = _time - m_lastTime;
+
+                // No difference in time so just return the same data index
+                if (timeDiff == 0.0f)
+                {
+                    return m_lastDataIndex;
+                }
+                // Positive difference in time means we moved forward so we should search that direction first
+                else if (timeDiff > 0.0f)
+                {
+                    // Search forward, including the current data point 
+                    int index = SearchForward(true, _time);
+
+                    // If it somehow wasn't found, now search backwards, but don't include the current point since we already searched it
+                    if (index == -1)
+                    {
+                        index = SearchBackward(false, _time);
+
+                        // If we STILL haven't found it, just return the current index again
+                        if (index == -1)
+                            return 0;
+                        else
+                            return index;
+                    }
+                    else
+                    {
+                        return index;
+                    }
+                }
+                // Negative difference in time means we moved backward so we should search that direction first
+                else if (timeDiff < 0.0f)
+                {
+                    // Search backward, including the current data point 
+                    int index = SearchBackward(true, _time);
+
+                    // If it somehow wasn't found, now search forwards, but don't include the current point since we already searched it
+                    if (index == -1)
+                    {
+                        index = SearchForward(false, _time);
+
+                        // If we STILL haven't found it, just return the first index
+                        if (index == -1)
+                            return 0;
+                        else
+                            return index;
+                    }
+                    else
+                    {
+                        return index;
+                    }
+                }
             }
 
-            // Return the selected index
-            return selectedIndex;
+            // If we get to here, just return 0
+            return 0;
+        }
+
+        private int SearchForward(bool _includeLastIndex, float _time)
+        {
+            // If we should include the last used index, start there. Otherwise, start at the next one
+            int startIndex = (_includeLastIndex) ? m_lastDataIndex : m_lastDataIndex + 1;
+
+            // Loop through and check all of the datapoints until we find a match
+            for (int i = startIndex; i < m_dataPoints.Count; i++)
+            {
+                if (CheckDataAtIndex(i, _time))
+                    return i;
+            }
+
+            // Return -1 if we couldn't find anything
+            return -1;
+        }
+
+        private int SearchBackward(bool _includeLastIndex, float _time)
+        {
+            // If we should include the last used index, start there. Otherwise, start at the previous one
+            int startIndex = (_includeLastIndex) ? m_lastDataIndex : m_lastDataIndex - 1;
+
+            // Loop through and check all of the datapoints until we find a match
+            for (int i = startIndex; i >= 0; i--)
+            {
+                if (CheckDataAtIndex(i, _time))
+                    return i;
+            }
+
+            // Return -1 if we couldn't find anything
+            return -1;
+        }
+
+        public bool CheckDataAtIndex(int _index, float _time)
+        {
+            if (_index >= m_dataPoints.Count - 1)
+                return false;
+
+            // Get the datapoint at the current index and next index
+            var thisDataPoint = m_dataPoints[_index];
+            var nextDataPoint = m_dataPoints[_index + 1];
+
+            // If this datapoint is BEFORE OR AT the time and the next one is AFTER the time, then we are at the right data point
+            return (thisDataPoint.m_timestamp <= _time && nextDataPoint.m_timestamp > _time);
         }
 
         public string GetTrackName()
--- ThesisV2/Assets/Thesis/My Assets/Scripts/VisTrack/VisTrack_Skeletal.cs
+++ ThesisV2/Assets/Thesis/My Assets/Scripts/VisTrack/VisTrack_Skeletal.cs
@@ -63,6 +63,8 @@ namespace Thesis.VisTrack
         private List<Data_Skeletal_Anim> m_animDataPoints;
         private List<Transform> m_boneObjs;
         private List<SkinnedMeshRenderer> m_skinnedMeshes;
+        private int m_lastDataIndex = 0;
+        private float m_lastTime = 0.0f;
 
 
 
@@ -160,26 +162,116 @@ namespace Thesis.VisTrack
         public int FindDataPointForTime(float _time)
         {
             // Ensure the datapoints are actually setup
-            Assert.IsNotNull(m_animDataPoints, "Track Assert Failed [" + GetTrackName() + "] - " + "m_animDataPoints has to be setup for before looking for a data point on object [" + this.gameObject.name + "]");
-            Assert.IsTrue(m_animDataPoints.Count >= 1, "Track Assert Failed [" + GetTrackName() + "] - " + "m_animDataPoints cannot be empty on object [" + this.gameObject.name + "]");
+            Assert.IsNotNull(m_animDataPoints, "Track Assert Failed [" + GetTrackName() + "] - " + "m_dataPoints has to be setup for before looking for a data point on object [" + this.gameObject.name + "]");
+            Assert.IsTrue(m_animDataPoints.Count >= 1, "Track Assert Failed [" + GetTrackName() + "] - " + "m_dataPoints cannot be empty on object [" + this.gameObject.name + "]");
 
-            // Start by setting the selected index to 0 in case there is only one point
-            int selectedIndex = 0;
-
-            // Loop through all of the data and find the nearest point BEFORE the given time
-            for (selectedIndex = 0; selectedIndex < m_animDataPoints.Count - 1; selectedIndex++)
+            if (m_animDataPoints.Count > 1)
             {
-                // Get the datapoint at the current index and next index
-                var thisDataPoint = m_animDataPoints[selectedIndex];
-                var nextDataPoint = m_animDataPoints[selectedIndex + 1];
+                // If we are PAST the last data point, return the final datapoint again
+                if (_time >= m_animDataPoints[m_animDataPoints.Count - 1].m_timestamp)
+                    return m_animDataPoints.Count - 1;
 
-                // If this datapoint is BEFORE OR AT the time and the next one is AFTER the time, then we are at the right data point
-                if (thisDataPoint.m_timestamp <= _time && nextDataPoint.m_timestamp > _time)
-                    break;
+                // Determine if we are going forward or backward in time
+                float timeDiff = _time - m_lastTime;
+
+                // No difference in time so just return the same data index
+                if (timeDiff == 0.0f)
+                {
+                    return m_lastDataIndex;
+                }
+                // Positive difference in time means we moved forward so we should search that direction first
+                else if (timeDiff > 0.0f)
+                {
+                    // Search forward, including the current data point 
+                    int index = SearchForward(true, _time);
+
+                    // If it somehow wasn't found, now search backwards, but don't include the current point since we already searched it
+                    if (index == -1)
+                    {
+                        index = SearchBackward(false, _time);
+
+                        // If we STILL haven't found it, just return the current index again
+                        if (index == -1)
+                            return 0;
+                        else
+                            return index;
+                    }
+                    else
+                    {
+                        return index;
+                    }
+                }
+                // Negative difference in time means we moved backward so we should search that direction first
+                else if (timeDiff < 0.0f)
+                {
+                    // Search backward, including the current data point 
+                    int index = SearchBackward(true, _time);
+
+                    // If it somehow wasn't found, now search forwards, but don't include the current point since we already searched it
+                    if (index == -1)
+                    {
+                        index = SearchForward(false, _time);
+
+                        // If we STILL haven't found it, just return the first index
+                        if (index == -1)
+                            return 0;
+                        else
+                            return index;
+                    }
+                    else
+                    {
+                        return index;
+                    }
+                }
             }
 
-            // Return the selected index
-            return selectedIndex;
+            // If we get to here, just return 0
+            return 0;
+        }
+
+        private int SearchForward(bool _includeLastIndex, float _time)
+        {
+            // If we should include the last used index, start there. Otherwise, start at the next one
+            int startIndex = (_includeLastIndex) ? m_lastDataIndex : m_lastDataIndex + 1;
+
+            // Loop through and check all of the datapoints until we find a match
+            for (int i = startIndex; i < m_animDataPoints.Count; i++)
+            {
+                if (CheckDataAtIndex(i, _time))
+                    return i;
+            }
+
+            // Return -1 if we couldn't find anything
+            return -1;
+        }
+
+        private int SearchBackward(bool _includeLastIndex, float _time)
+        {
+            // If we should include the last used index, start there. Otherwise, start at the previous one
+            int startIndex = (_includeLastIndex) ? m_lastDataIndex : m_lastDataIndex - 1;
+
+            // Loop through and check all of the datapoints until we find a match
+            for (int i = startIndex; i >= 0; i--)
+            {
+                if (CheckDataAtIndex(i, _time))
+                    return i;
+            }
+
+            // Return -1 if we couldn't find anything
+            return -1;
+        }
+
+        public bool CheckDataAtIndex(int _index, float _time)
+        {
+            if (_index >= m_animDataPoints.Count - 1)
+                return false;
+
+            // Get the datapoint at the current index and next index
+            var thisDataPoint = m_animDataPoints[_index];
+            var nextDataPoint = m_animDataPoints[_index + 1];
+
+            // If this datapoint is BEFORE OR AT the time and the next one is AFTER the time, then we are at the right data point
+            return (thisDataPoint.m_timestamp <= _time && nextDataPoint.m_timestamp > _time);
         }
 
         public string GetTrackName()
